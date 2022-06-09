e75911457103969746366bc406156a33461bc6ce
DmitriyKlaes <89119584059@mail.ru>
2022-06-09 11:34:12 +0300

    task066-SummNumbersFromTo

---
+++ task066-SummNumbersFromTo/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNaturalElementsBetween(int firstNumber, int secondNumber)
+{
+    if (firstNumber < secondNumber)
+    {
+        return firstNumber + SumNaturalElementsBetween(firstNumber + 1, secondNumber);
+    }
+    if (firstNumber > secondNumber)
+    {
+        return secondNumber + SumNaturalElementsBetween(firstNumber, secondNumber + 1);
+    }
+    return firstNumber;
+}
+
+int numberM = 1;
+int numberN = 15;
+int result = SumNaturalElementsBetween(numberM, numberN);
+Console.WriteLine($"Сумма натуральных элементов между {numberM} и {numberN} = {result}");
+
+// M = 1; N = 15 ---> 120
+// M = 15; N = 1 ---> 120
